27f79d568cce829481e977d39876551e96a08393
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 10:27:07 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// Get started
+
+const name = prompt("What's your name?")
+
+
+console.log("Welcome to Murder Mystery, " + name)
+
+let intro = prompt("Choose you role, (Murderer, Sheriff, Innocent)")
+
+console.log(`Okay ${name}, you are Murderer, but make sure to not tell anybody your role`)
+
+
+let number = 3
+
+console.log(`You have killed ${number} people and you have 2 minutes left before innocents win`)
+
+ 
+
+console.log(`CONGRADULATIONS you killed all ${number} people and you have won the game!`)
+
+
+
